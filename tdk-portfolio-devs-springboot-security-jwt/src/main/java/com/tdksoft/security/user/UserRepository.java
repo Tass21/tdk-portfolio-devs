0ca12a04be629c2f2e7c93cf7b4563df78d16fdf
H.Kengne <tdksoftconsulting@gmail.com>
2024-02-19 22:50:06 +0100

    User repository added

--- tdk-portfolio-devs-springboot-security-jwt/src/main/java/com/tdksoft/security/user/UserRepository.java
+++ tdk-portfolio-devs-springboot-security-jwt/src/main/java/com/tdksoft/security/user/UserRepository.java
@@ -2,7 +2,11 @@ package com.tdksoft.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    public Optional<User> findByEmail(String email);
+
 }
